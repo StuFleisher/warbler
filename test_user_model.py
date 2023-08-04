bf9659dc302ee4b1b0b671f10f24b9eb753d73e1
Stuart Fleisher <stufleisher@gmail.com>
2023-08-03 20:49:09 -0400

    added a few tests

--- test_user_model.py
+++ test_user_model.py
@@ -8,7 +8,7 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follow
+from models import db, User, Message, Follow, Like
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -20,6 +20,16 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 # Now we can import app
 
 from app import app
+
+
+DEFAULT_IMAGE_URL = (
+    "https://icon-library.com/images/default-user-icon/" +
+    "default-user-icon-28.jpg")
+
+DEFAULT_HEADER_IMAGE_URL = (
+    "https://images.unsplash.com/photo-1519751138087-5bf79df62d5b?ixlib=" +
+    "rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=for" +
+    "mat&fit=crop&w=2070&q=80")
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -49,3 +59,64 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
+
+    def test_user_following(self):
+        """Does is_following successfully detect when user1 is following user2?
+        Does is_followed_by successfully detect when user2 is followed by user1?
+        """
+
+        u1=User.query.get(self.u1_id)
+        u2=User.query.get(self.u2_id)
+
+        follow = Follow(user_being_followed_id=self.u2_id,
+                        user_following_id=self.u1_id )
+
+        db.session.add(follow)
+        db.session.commit()
+
+
+        self.assertTrue(u1.is_following(u2))
+        self.assertTrue(u2.is_followed_by(u1))
+
+    def test_user_not_following(self):
+        """Does is_following successfully detect when user1 is not following
+          user2?
+          Does is_followed_by successfully detect when user2 is not followed
+          by user1?
+        """
+
+        u1=User.query.get(self.u1_id)
+        u2=User.query.get(self.u2_id)
+
+        self.assertFalse(u1.is_following(u2))
+        self.assertFalse(u2.is_followed_by(u1))
+
+    def test_valid_signup(self):
+        """Does User.signup successfully create a new user given
+        valid credentials?"""
+
+        self.assertEqual(User.query.count(),2)
+        u3= User.signup('u3',"u3@email.com", "password", None)
+
+        self.assertEqual(User.query.count(),3)
+        self.assertEqual(u3.username,'u3')
+        self.assertEqual(u3.email,'u3@email.com')
+        self.assertEqual(u3.image_url, DEFAULT_IMAGE_URL)
+        self.assertIn('$2b',u3.password)
+
+        # signup(cls, username, email, password, image_url=DEFAULT_IMAGE_URL):
+
+    def test_invalid_signup(self):
+        """ Does User.signup fail to create a new user if any of the
+        validations (eg uniqueness, non-nullable fields) fail?"""
+
+        ...
+#
+#
+#
+#
+#
+# Does User.signup fail to create a new user if any of the validations (eg uniqueness, non-nullable fields) fail?
+# Does User.authenticate successfully return a user when given a valid username and password?
+# Does User.authenticate fail to return a user when the username is invalid?
+# Does User.authenticate fail to return a user when the password is invalid?
--- todo.md
+++ todo.md
@@ -1,13 +1,7 @@
-1. Add UI to 'click' on like
-    - each message requires a form to like,
 
-2. Add logic to like
-    - show diff form if message is liked/not liked
-    - show diff icon if message is liked/not liked
+Model tests
 
-3. Add logic to unlike post
-
-4. Page to show all user likes
-
-5. Display everyone whose liked a message on the messages page
-
+User
+Message
+Like
+Follow
